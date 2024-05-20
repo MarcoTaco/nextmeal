e5f8491003c7999711b991b3ff27654cba5b37a6
Marco <marco.a.romero0303@gmail.com>
2024-05-19 19:14:27 -0700

    first commit for food selection page

---
+++ nextmeal/src/pages/foodSelection.tsx
@@ -0,0 +1,11 @@
+import "../styles/foodSelection.scss";
+
+function FoodSelection(){
+    <>
+        
+    </>
+}
+
+export default FoodSelection;
+
+// this page is for the food selection. imagine the retail stores how there's a page to "window shop"
